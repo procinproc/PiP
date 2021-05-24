c878dbddec2f54f530d30fdced10ca739747af50
Atsushi Hori <ahori@riken.jp>
2021-05-24 14:54:22 +0900

    for doing test of test.sh (timer)

--- .github/workflows/test.sh
+++ .github/workflows/test.sh
@@ -8,6 +8,7 @@ time make install &&
 
 cd /host/PiP-Testsuite &&
 ./configure --with-pip=$HOME/pip &&
-:
+PIP_TEST_THRESHOLD=10 make test
+#:
 ### FIXME:
 ### the above line should be "time make test" instead of ":", but it hangs
